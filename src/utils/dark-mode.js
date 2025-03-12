bda7733c817623d895ceb3d1b2d408a88f7bcb43
W3bm@steR <clarence.wagontec.dev@gmail.com>
2025-03-12 17:36:04 +0200

    Enhance theme switching and correct public dir file fetching

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,12 +1,12 @@
 const darkMode = () => {
     const headerSun = document.querySelector('.header__sun')
     const theme = localStorage.getItem('theme')
-    theme && document.body.classList.toggle('light-mode')
+    theme && document.body.classList.toggle('dark-mode')
 
     headerSun.addEventListener('click', () => {
-        document.body.classList.toggle('light-mode')
-        if(document.body.classList.contains('light-mode')){
-            localStorage.setItem('theme', 'light-mode')
+        document.body.classList.toggle('dark-mode')
+        if(document.body.classList.contains('dark-mode')){
+            localStorage.setItem('theme', 'dark-mode')
         }else{
             localStorage.removeItem('theme')
             document.body.removeAttribute('class')
